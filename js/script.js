538a6e9e82a57964aaac79a2d5575ce765be30d2
Reiker <reiker1@hotmail.com>
2023-03-30 02:38:12 +0200

    firstCommit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('.navbar')
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+})
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
+
+var swiper = new Swiper(".gallery-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
